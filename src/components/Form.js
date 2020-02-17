c3a9a534d80d5e300fff070466a021a25f38b69f
Gulshan Saini <Gulshan.Saini@exponential.com>
2020-02-17 22:28:26 +0530

    lifted keyword state up

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,19 @@
 import React from "react";
 
-const Form = () => {
+const Form = props => {
+  const { keyword, handleChange } = props;
+
   return (
     <form className="searchForm">
       <h1>Enter FIFA Code</h1>
-      <div class="search-controls">
-        <input type="text" value="" placeholder="FIFA country code" required />
+      <div className="search-controls">
+        <input
+          type="text"
+          value={keyword}
+          onChange={handleChange}
+          placeholder="FIFA country code"
+          required
+        />
         <button type="submit">Search</button>
       </div>
     </form>
--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import Form from "./components/Form";
 import "./index.css";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ keyword: e.target.value });
+  };
+
   render() {
     return (
       <div className="container">
-        <Form />
+        <Form keyword={this.state.keyword} handleChange={this.handleChange} />
       </div>
     );
   }
